5d9c8cba19b41023ffba58d727a5c4cb2df5ea89
Teo <130609905+teom275@users.noreply.github.com>
2023-04-13 18:24:41 -0300

    Create necessary libraries.py

---
+++ necessary libraries.py
@@ -0,0 +1,9 @@
+#importing necessary libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.patches import Circle
+
+#setting plot style to seaborn
+plt.style.use('seaborn')
